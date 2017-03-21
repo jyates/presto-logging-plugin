64b92a80043fa0444c3ea9774f52c8abcabec986
Jack McCracken <jack.mccracken@shopify.com>
2017-03-21 13:18:08 -0400

    Add event-listeners and install oracle java

--- packages/presto-logging-plugin-shopify.rb
+++ packages/presto-logging-plugin-shopify.rb
@@ -7,6 +7,11 @@ deb do
 
     build do
         source git: "https://github.com/Shopify/presto-logging-plugin.git", branch: "v#{@version}"
+        run "apt-get install -y software-properties-common"
+        run "add-apt-repository 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main'"
+        run "apt-get update"
+        run "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections"
+        run "apt-get -y install oracle-java8-installer maven"
         run "mvn install"
         run "install -d 0700 #{deb_dir}/u/apps/presto/current/plugin/logging-plugin"
         run "install target/logging-plugin-#{@version}.jar #{deb_dir}/u/apps/presto/current/plugin/logging-plugin"
